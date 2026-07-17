d9f88e392a3b4bd6008fb3f378c22bfcd2ab6e5d
Romulo Gomes <romulo.gomes@cscglobal.com>
2026-07-17 16:38:08 -0300

    Refatoração do projeto para melhor organização dos testes. Criação do caso de teste API-CT001

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,150 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { faker } from '@faker-js/faker';
+
+Cypress.Commands.add('criaApiAdminUserELoga', () => {
+    cy.request({
+        method: 'POST',
+        url: `${Cypress.expose('apiUrl')}/login`,
+        body: {
+            email: 'automation@serverest.dev',
+            password: 'teste'
+        },
+        failOnStatusCode: false,
+    }).then((response) => {
+        if (response.status === 200) {
+            expect(response.body.authorization).to.be.not.empty;
+            Cypress.expose('adminToken', response.body.authorization);
+        } else {
+            cy.request({
+                method: 'POST',
+                url: `${Cypress.expose('apiUrl')}/usuarios`,
+                body: {
+                    nome: 'Automation',
+                    email: 'automation@serverest.dev',
+                    password: 'teste',
+                    administrador: 'true',
+                },
+            }).then((response) => {
+                Cypress.expose('userId', response.body._id);
+                cy.request({
+                    method: 'POST',
+                    url: `${Cypress.expose('apiUrl')}/login`,
+                    body: {
+                        email: 'automation@serverest.dev',
+                        password: 'teste'
+                    },
+                }).then((response) => {
+                    expect(response.body.authorization).to.be.not.empty;
+                    Cypress.expose('adminToken', response.body.authorization);
+                });
+            });
+        }
+    })
+})
+
+Cypress.Commands.add('criaUsuarioRandomicoELoga', () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    cy.request({
+        method: 'POST',
+        url: `${Cypress.expose('apiUrl')}/usuarios`,
+        body: {
+            nome: faker.person.fullName(),
+            email: email,
+            password: password,
+            administrador: 'false',
+        },
+    }).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body.message).to.eq('Cadastro realizado com sucesso');
+        Cypress.expose('userId', response.body._id);
+        cy.request({
+            method: 'POST',
+            url: `${Cypress.expose('apiUrl')}/login`,
+            body: {
+                email: email,
+                password: password,
+            },
+        }).then((response) => {
+            Cypress.expose('userToken', response.body.authorization);
+        });
+    })
+})
+
+Cypress.Commands.add('criaProdutoRandomico', () => {
+    const quantity = faker.number.int({ min: 10, max: 20 });
+    cy.request({
+        method: 'POST',
+        url: `${Cypress.expose('apiUrl')}/produtos`,
+        headers: {
+            Authorization: `${Cypress.expose('adminToken')}`,
+        },
+        body: {
+            nome: faker.commerce.productName(),
+            preco: faker.number.int({ min: 10, max: 100 }),
+            descricao: faker.commerce.productDescription(),
+            quantidade: quantity,
+        }
+    }).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body.message).to.eq('Cadastro realizado com sucesso');
+        Cypress.expose('productId', response.body._id);
+        Cypress.expose('productQuantity', quantity);
+    })
+})
+
+Cypress.Commands.add('criaCarrinho', () => {
+    cy.request({
+        method: 'POST',
+        url: `${Cypress.expose('apiUrl')}/carrinhos`,
+        headers: {
+            Authorization: `${Cypress.expose('userToken')}`,
+        },
+        body: {
+            produtos: [
+                {
+                    idProduto: Cypress.expose('productId'),
+                    quantidade: 1,
+                }
+            ]
+        }
+    }).then((response) => {
+        expect(response.status).to.eq(201);
+        expect(response.body.message).to.eq('Cadastro realizado com sucesso');
+        Cypress.expose('cartId', response.body._id);
+    })
+})
+
+Cypress.Commands.add('verificaQueCarrinhoFoiCriado', () => {
+    cy.log(Cypress.expose('userId'));
+    cy.request({
+        method: 'GET',
+        url: `${Cypress.expose('apiUrl')}/carrinhos`,
+        qs: {
+            idUsuario: Cypress.expose('userId'),
+        },
+        headers: {
+            Authorization: `${Cypress.expose('userToken')}`,
+        },
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.quantidade).to.be.eq(1);
+        expect(response.body.carrinhos[0].produtos[0].idProduto).to.eq(Cypress.expose('productId'));
+    })
+})
+
+Cypress.Commands.add('verificaQueOEstoqueFoiAtualizado', () => {
+    cy.request({
+        method: 'GET',
+        url: `${Cypress.expose('apiUrl')}/produtos/${Cypress.expose('productId')}`,
+        headers: {
+            Authorization: `${Cypress.expose('adminToken')}`,
+        },
+    }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.quantidade).to.be.eq(Cypress.expose('productQuantity') - 1);
+    })
+})
