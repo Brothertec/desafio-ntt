a2f4a30c57e84db06448460ff7ff13db3aa24b51
Romulo Gomes <romulo.gomes@cscglobal.com>
2026-07-17 11:47:42 -0300

    Criação de esqueleto de testes de api

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,11 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: 'https://front.serverest.dev/',
+    expose: {
+      apiUrl: 'https://serverest.dev',
+    },
+    specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
