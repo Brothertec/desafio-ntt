c537cd46afe53564fdcd6a46612068decce233fb
Romulo Gomes <romulo.gomes@cscglobal.com>
2026-07-20 03:21:51 -0300

    Setup Prettier e adiciona no CI

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 const pluginCypress = require('eslint-plugin-cypress')
+const eslintConfigPrettier = require('eslint-config-prettier')
 
 module.exports = [
   pluginCypress.configs.recommended,
+  eslintConfigPrettier,
 ]
